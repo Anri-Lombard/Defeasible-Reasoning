f35ed65a1f42483573677fa4a6d9466bbbe3fbd2
Anri Lombard <anri.m.lombard@gmail.com>
2022-08-09 15:08:42 +0200

    Theorem and proof

---
+++ components/Theorem.tsx
@@ -0,0 +1,10 @@
+const Theorem = () => {
+    return (
+        <div>
+            <span>Theorem</span>
+            <span>Proof</span>
+        </div>
+    )
+}
+
+export default Theorem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Theorem from '../components/Theorem'
 
 const Home: NextPage = () => {
   return (
@@ -14,49 +15,69 @@ const Home: NextPage = () => {
 
       <div>
         {/* TODO: Make links to places on page */}
-        <h1>Defeasible Reasoning</h1>
+        <h1 className={styles.mainHeader}>Defeasible Reasoning</h1>
         <div className={styles.contents}>
+          <div className={styles.contentsText}>
+            <h3 className={styles.contentsHeader}>Content</h3>
+            <ol>
+              <span>Propositional logic</span>
+              <ul>
+                <li>Syntax</li>
+                <li>Symantics</li>
+                <li>Deductive Systems</li>
+                <li>Consequence Relations</li>
+                <li>Classical Deduction</li>
+              </ul>
+              <span>Approaches to Defeasible Reasoning</span>
+              <ul>
+                <li>Belief Revision</li>
+                <li>Circumscription</li>
+                <li>Default Logic</li>
+                <li>Propositional Typicality Logic</li>
+                <li>Nonmonotonic Modal Logic</li>
+                <li>Autoepistemic Logic</li>
+                <li>Preferential Approach</li>
+              </ul>
+              <span>Preferential Reasoning</span>
+              <ul>
+                <li>Preferential Consequence Relations</li>
+                <li>Preferential Interpretations</li>
+                <li>Ranked Interpretations</li>
+                <li>Preferential Entailment</li>
+                <li>Ranked Entailment</li>
+              </ul>
+              <span>Nonmonotonic Reasoning</span>
+              <ul>
+                <li>Minimal Ranked Entailment</li>
+                <li>Rational Closure</li>
+                <li>Algorithm for Rational Closure</li>
+                <li>Rational Entailment</li>
+              </ul>
+            </ol>
+          </div>
+        </div>
+        <section className={styles.section}>
+          <h1>Propositional Logic</h1>
 
-          <h3 className={styles.contentsHeader}>Content</h3>
-          <ol>
-            <span>Propositional logic</span>
-            <ul>
-              <li>Syntax</li>
-              <li>Symantics</li>
-              <li>Deductive Systems</li>
-              <li>Consequence Relations</li>
-              <li>Classical Deduction</li>
-            </ul>
-            <span>Approaches to Defeasible Reasoning</span>
-            <ul>
-              <li>Belief Revision</li>
-              <li>Circumscription</li>
-              <li>Default Logic</li>
-              <li>Propositional Typicality Logic</li>
-              <li>Nonmonotonic Modal Logic</li>
-              <li>Autoepistemic Logic</li>
-              <li>Preferential Approach</li>
-            </ul>
-            <span>Preferential Reasoning</span>
-            <ul>
-              <li>Preferential Consequence Relations</li>
-              <li>Preferential Interpretations</li>
-              <li>Ranked Interpretations</li>
-              <li>Preferential Entailment</li>
-              <li>Ranked Entailment</li>
-            </ul>
-            <span>Nonmonotonic Reasoning</span>
-            <ul>
-              <li>Minimal Ranked Entailment</li>
-              <li>Rational Closure</li>
-              <li>Algorithm for Rational Closure</li>
-              <li>Rational Entailment</li>
-            </ul>
-          </ol>
-        </div>
-        <div>
-          Atoms <span className="hover:scale-115 m-5">this</span>
-        </div>
+          <div>
+            <h3>Syntax</h3>
+            <Theorem></Theorem>
+          </div>
+          <div>
+            <h3>Symantics</h3>
+
+          </div>
+          <div>
+            <h3>Deductive Systems</h3>
+
+          </div>
+          <div>
+            <h3>Consequence Relations</h3>
+          </div>
+          <div>
+            <h3>Classical Deduction</h3>
+          </div>
+        </section>
       </div>
     </div>
   )
