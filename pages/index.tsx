b8fe775efa3f8d86c3663c5db90a47f64a94c688
Anri Lombard <anri.m.lombard@gmail.com>
2022-09-05 10:36:32 +0200

    Theory progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,10 +68,8 @@ const Home: NextPage = () => {
               <span>Key Concepts</span>
               <ul>
                 <li>Non-monotonic Reasoning</li>
-                <li>Minimal Ranked Entailment</li>
                 <li>Rational Closure</li>
-                <li>Algorithm for Rational Closure</li>
-                <li>Defeasible Entailment</li>
+                <li>Algorithms for Rational Closure</li>
               </ul>
               <span>Tool Walkthrough</span>
               <ul>
@@ -99,7 +97,9 @@ const Home: NextPage = () => {
             to the natural intelligence displayed by animals including humans. AI research has
             been defined as the field of study of intelligent agents, which refers to any system
             that perceives its environment and takes actions that maximize its chance of achieving
-            its goals - <strong>Wikipedia</strong>
+            its goals - <a href="https://en.wikipedia.org/wiki/Artificial_intelligence" target="_blank" rel="noreferrer">
+              Wikipedia
+            </a>
           </div>
 
           <h3 className={styles.sectionHeading}>1.2 Knowledge Representation</h3>
@@ -116,16 +116,27 @@ const Home: NextPage = () => {
             design formalisms that will make complex systems easier to design and build. Knowledge
             representation and reasoning also incorporates findings from logic to automate various
             kinds of reasoning, such as the application of rules or the relations of sets and subsets
-            - <strong>Wikipedia</strong>
+            - <a href="https://en.wikipedia.org/wiki/Knowledge_representation_and_reasoning" target="_blank" rel="noreferrer">
+              Wikipedia
+            </a>
           </div>
 
           <h3 className={styles.sectionHeading}>1.3 Defeasible Reasoning</h3>
           <div className={styles.videos}>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/Yf_S4d2BU4M" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </div>
-          {/* <div className={styles.sectionSpan}>
-
-          </div> */}
+          <div className={styles.sectionSpan}>
+            Defeasible reasoning is an important form of non-monotonic reasoning because it introduces mechanisms for 
+            strengthening and weakening new information presented to a reasoner. Defeasible reasoning also deals with 
+            atypical reasoning situations in which humans inherently reason differently, whether due to their beliefs, 
+            context or other circumstances. Defeasible reasoning and its mechanisms allow AI systems to represent
+            human reasoning more accurately than classical reasoning and more accurately than the basic notion of 
+            non-monotonicreasoning.
+            <br /><br />
+            <a href="https://projects.cs.uct.ac.za/honsproj/cgi-bin/view/2021/everett_morris.zip/resources/Project_Poster.pdf">
+              Propositional Defeasible Explanation
+            </a>
+          </div>
         </section>
         <section className={styles.section}>
           <h1>2. Key Concepts</h1>
@@ -133,24 +144,45 @@ const Home: NextPage = () => {
           <div>
             <h3 className={styles.sectionHeading}>2.1 Non-monotonic Reasnoning</h3>
             <div className={styles.sectionSpan}>
-              <strong>Nonmonotonic reasoning</strong> is an area of research that attempts to formalize different patterns of 
-              “common sense” reasoning, by dropping monotonicity as property and investigating how to define 
-              reasonable notions of consequences. Humans reason by making assumptions based on the knowledge they have, 
-              and then revising those assumptions upon learning new information. Several frameworks were formalized to 
-              mimic this pattern known as defeasible reasoning
+              <strong>Nonmonotonic reasoning</strong> is an area of research that attempts to formalize different patterns of
+              “common sense” reasoning, by dropping monotonicity as a property and investigating how to define
+              reasonable notions of consequences. Humans reason by making assumptions based on the knowledge they have,
+              and then revising those assumptions upon learning new information. Several frameworks were formalized to
+              mimic this pattern known as <strong>defeasible reasoning</strong>.
+              <br /><br />
+              <em>
+                Nonmonotonic reasoning is a subfield of Artificial Intelligence trying to find more realistic
+                formal models of reasoning than classical logic. In common sense reasoning one often draws conclusions
+                that have to be withdrawn when further information is obtained. The set of conclusions thus does not
+                grow monotonically with the given information. It is this phenomenon that nonmonotonic reasoning methods
+                try to formalize. This volume gives an overview on recent results in the field and points to relevant
+                literature for further study.
+              </em> - <a href="https://web.stanford.edu/group/cslipublications/cslipublications/site/1881526836.shtml" target="_blank" rel="noreferrer">
+                Stanford University
+              </a>
             </div>
           </div>
           <div>
-            <h3 className={styles.sectionHeading}>2.2 Minimal Ranked Entailment</h3>
+            <h3 className={styles.sectionHeading}>2.2 Rational Closure</h3>
+            <div className={styles.sectionSpan}>
+              A particularly appreciated non-monotonic consequence relation is represented by the class of <strong>
+                rational consequence relations</strong>. A consequence relation |∼ is rational iff it satisfies the 
+              following properties:
+              <br /><br />
+              <Image className="rounded-lg" src="/images/RationalClosureProperties1.jpeg" alt="final result" layout="responsive" priority width={100} height={17} />
+              <Image className="rounded-lg" src="/images/RationalClosureProperties2.jpeg" alt="final result" layout="responsive" priority width={100} height={40} />
+              <br />
+              Each postulate corresponds to a pattern of reasoning about <strong>Defeasible Information</strong>.
+              <br /><br />
+              Here is a more <a href="https://www.cair.org.za/sites/default/files/2022-02/Explanation_For_KLM.pdf" target="_blank" rel="noreferrer">
+                in-depth explanation</a>.
+            </div>
           </div>
           <div>
-            <h3 className={styles.sectionHeading}>2.3 Rational Closure</h3>
-          </div>
-          <div>
-            <h3 className={styles.sectionHeading}>2.4 Algorithm for Rational Closure</h3>
-          </div>
-          <div>
-            <h3 className={styles.sectionHeading}>2.5 Defeasible Entailment</h3>
+            <h3 className={styles.sectionHeading}>2.3 Algorithm for Rational Closure</h3>
+            <div className={styles.sectionSpan}>
+              
+            </div>
           </div>
         </section>
         <section className={styles.section}>
@@ -169,11 +201,10 @@ const Home: NextPage = () => {
               <strong>Step 1</strong>: Download the Defeasible Reasoning GUI <br /><br />
               <strong>Step 2</strong>: Add own database along with defeasible query in the form of a .txt file (see section 3.2)<br /><br />
               <strong>Step 3</strong>: Enter the database name without the .txt extension and choose the entailment checker algorithm you need.
-              - Regular simply outputs the process whereby Binary outputs &ldquo;true&rdquo; or &ldquo;false&rdquo;
+              - Regular simply outputs the process whereby Binary outputs &ldquo;true&rdquo; or &ldquo;false&rdquo; <br /><br />
               <strong>Step 4</strong>: Grab a cup of coffee and relax, the rest will be done for you. <br /><br />
               <figure className="p-10 border-2 border-white">
-                <Image className="rounded-lg" src="/images/FinalResult.png" alt="final result" layout="responsive" priority width={100} height={100} />
-                <figcaption>Final Result</figcaption>
+                <Image className="rounded-lg" src="/images/FinalResult.png" alt="final result" layout="responsive" priority width={100} height={90} />
               </figure>
             </div>
           </div>
